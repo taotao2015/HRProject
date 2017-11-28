902ae7a292b6d79e62c75f21d8f04acfac5504ff
wangliang <wangx580@ziroom.com>
2017-11-28 16:35:21 +0800

    ZRHRP

---
+++ HRProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HRProject
+//
+//  Created by wangliang on 2017/11/28.
+//  Copyright © 2017年 ziroom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HRProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HRProject
+//
+//  Created by wangliang on 2017/11/28.
+//  Copyright © 2017年 ziroom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
